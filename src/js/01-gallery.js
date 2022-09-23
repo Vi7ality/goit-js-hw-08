e744cd225525db42745295dea29bafc814943ca0
Vitalii <svitalii138@gmail.com>
2022-09-24 00:22:22 +0300

    HW #1 Done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+    return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}"  alt="${description}"/>
+</a>`
+    })
+    .join('')
+
+};
+
+const lightBox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250
+});
