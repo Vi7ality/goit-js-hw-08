559b344fa25a74c96f3db4dcfb5ab2bf2303cd33
Vitalii <svitalii138@gmail.com>
2022-09-24 23:37:42 +0300

    HW #2 is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+    const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
+
+initPlayer();
+
+const onPlay = throttle(getCurrentVideoTime, 1000)
+    function getCurrentVideoTime (currentTime) {
+ localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(currentTime))
+}
+
+player.on('timeupdate', onPlay);
+
+
+function initPlayer() {
+    const localVideoTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const localParcedTime = JSON.parse(localVideoTime);
+    if (localVideoTime) {
+        player.setCurrentTime(localParcedTime.seconds);
+    }
+}
+
+
+
 
